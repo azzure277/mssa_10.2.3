e45d973d553772cfb7a4aceb55a55a4f9ad45b7f
azzure277 <brandon.govender21@outlook.com>
2025-09-10 15:07:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace mssa_10._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            Console.Write("Input starting character for the string : ");
+            char startChar = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            Console.Write("Input ending character for the string : ");
+            char endChar = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            var city = cities.FirstOrDefault(c => c.StartsWith(startChar) && c.EndsWith(endChar));
+
+            if (city != null)
+            {
+                Console.WriteLine($"The city starting with {startChar} and ending with {endChar} is : {city}");
+            }
+            else
+            {
+                Console.WriteLine($"No city found starting with {startChar} and ending with {endChar}.");
+            }
+        }
+    }
+}
